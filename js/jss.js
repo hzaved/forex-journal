3133e7e438efb6694072100db2f05c2160f4850a
Nogunaa <nogunaa@gmail.com>
2021-12-13 14:51:12 -0700

    Fixed bootstrap mismatch between 4 and 5

--- js/jss.js
+++
@@ -1,8 +0,0 @@
-// const toggleButton = document.getElementsByClassName("dropdown-toggle")[0];
-// console.log("toggleButton", toggleButton);
-// const dropdown = document.getElementsByClassName("dropdown-menu")[0];
-// console.log("dropdown", dropdown);
-// toggleButton.addEventListener("click", function() {
-//     dropdown.classList.toggle("active");
-//     console.log("click");
-// });
