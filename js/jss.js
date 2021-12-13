0bb158c4927a40149940f4a3cd3833ffb37c4806
Nogunaa <nogunaa@gmail.com>
2021-12-13 14:37:06 -0700

    Adding project files

---
+++ js/jss.js
@@ -0,0 +1,8 @@
+// const toggleButton = document.getElementsByClassName("dropdown-toggle")[0];
+// console.log("toggleButton", toggleButton);
+// const dropdown = document.getElementsByClassName("dropdown-menu")[0];
+// console.log("dropdown", dropdown);
+// toggleButton.addEventListener("click", function() {
+//     dropdown.classList.toggle("active");
+//     console.log("click");
+// });
